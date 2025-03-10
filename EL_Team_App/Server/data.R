f64a498e2262cdc887d0e8c3397d7b5e6541b9f8
YigitAsik <yyigitasikk@gmail.com>
2025-03-10 21:18:30 +0300

    Added defensive tabs

--- EL_Team_App/Server/data.R
+++ EL_Team_App/Server/data.R
@@ -80,3 +80,86 @@ output$team_off_byzone <- renderDataTable({
   )
   
 })
+
+##### Team D: TRADITIONAL #####
+
+output$team_def_traditional <- renderDataTable({
+  
+  datatable(
+    traditional_def_agg(),
+    class = 'display compact order-column', # no wrap
+    filter = 'top',
+    rownames = F,
+    
+    options = list(
+      paging = T,
+      pageLength = 25,
+      autoWidth = T,
+      scrollX = T
+    )
+  )
+  
+})
+
+##### Team D: ADVANCED #####
+
+output$team_def_advanced <- renderDataTable({
+  
+  
+  datatable(
+    advanced_def_agg(),
+    class = 'display compact order-column', # no wrap
+    filter = 'top',
+    rownames = F,
+    
+    options = list(
+      paging = T,
+      pageLength = 25,
+      autoWidth = T,
+      scrollX = T
+    )
+  )
+  
+})
+
+##### Team D: SCORING #####
+
+output$team_def_scoring <- renderDataTable({
+  
+  
+  datatable(
+    scoring_def_agg(),
+    class = 'display compact order-column', # no wrap
+    filter = 'top',
+    rownames = F,
+    
+    options = list(
+      paging = T,
+      pageLength = 25,
+      autoWidth = T,
+      scrollX = T
+    )
+  )
+  
+})
+
+##### Team D: By Zone #####
+
+output$team_def_byzone <- renderDataTable({
+  
+  
+  datatable(
+    byzone_def_agg(),
+    class = 'display compact order-column', # no wrap
+    filter = 'top',
+    rownames = F,
+    
+    options = list(
+      paging = T,
+      pageLength = 25,
+      autoWidth = T,
+      scrollX = T
+    )
+  )
+  
+})
--- EL_Team_App/Server/reactiveData.R
+++ EL_Team_App/Server/reactiveData.R
@@ -26,3 +26,32 @@ byzone_off_agg <- reactive({
     mutate(SHOT_ZONE_BASIC = as.factor(SHOT_ZONE_BASIC),
            SHOT_ZONE_AREA = as.factor(SHOT_ZONE_AREA))
 })
+
+# TEAM DEF STATS
+
+traditional_def_agg <- reactive({
+  read.csv('Data/traditional_def_df_20250310.csv') %>%
+    select(-X)
+})
+
+advanced_def_agg <- reactive({
+  read.csv('Data/advanced_def_df_20250310.csv') %>%
+    select(-X) %>%
+    mutate(POSS_IMP = factor(POSS_IMP,
+                             c('Low', 'Medium', 'High', 'Very High', 'All'))) %>%
+    arrange(Team, -POSS)
+})
+
+scoring_def_agg <- reactive({
+  read.csv('Data/scoring_def_df_20250310.csv') %>%
+    select(-X) %>%
+    mutate(POSS_IMP = factor(POSS_IMP,
+                             c('Low', 'Medium', 'High', 'Very High')))
+})
+
+byzone_def_agg <- reactive({
+  read.csv('Data/byzone_def_df_20250310.csv') %>%
+    select(-X) %>%
+    mutate(SHOT_ZONE_BASIC = as.factor(SHOT_ZONE_BASIC),
+           SHOT_ZONE_AREA = as.factor(SHOT_ZONE_AREA))
+})
